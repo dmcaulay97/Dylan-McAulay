43473934b0a7c625a3be2b7016f9463abcf74840
Dylan McAulay <dmcaulay97@gmail.com>
2021-11-16 12:41:58 -0500

    Added landing image

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,17 @@
 import React from "react";
+import img1 from "../img/dylan.png"
+import LandingCSS from "../css/landing.module.css"
 
 function Landing() {
     return (
-        <div>
-            This is a Landing Page
-        </div>
+        <section className={LandingCSS.container}>
+            <div className={LandingCSS.card}>
+
+            </div>
+            <div className={LandingCSS.card2}>
+
+            </div>
+        </section>
     )
 }
 
