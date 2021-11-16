7375f6a7c45f0db258d0eca904790a5e76137ce8
Dylan McAulay <dmcaulay97@gmail.com>
2021-11-16 14:32:13 -0500

    added fade loading for landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LandingCSS from "../css/landing.module.css"
 
+
+
 function Landing() {
+
+    useEffect(() => {
+        const hidden = document.getElementsByClassName(LandingCSS.lazy)
+        for (let i = 0; i < hidden.length; i++) {
+            const num = i.toString();
+            hidden[num].classList.add(LandingCSS.appear);
+        }
+
+        const overlay = document.getElementsByClassName(LandingCSS.overlay)
+        overlay[0].className = `${LandingCSS.appear} ${LandingCSS.hidden} ${LandingCSS.overlay}`
+
+
+
+    })
+
     return (
         <section className={LandingCSS.container}>
-            <div className={LandingCSS.card}>
-
+            <div className={LandingCSS.card1}>
+                <div className={`${LandingCSS.appear} ${LandingCSS.overlay}`}></div>
             </div>
             <div className={LandingCSS.card2}>
-                Dylan McAulay
+                <div className={`${LandingCSS.title} ${LandingCSS.lazy}`}>
+                    Dylan McAulay
+                </div>
+                <div className={`${LandingCSS.button} ${LandingCSS.lazy}`}>
+                    Contact Me
+                </div>
+                <div className={`${LandingCSS.links} ${LandingCSS.lazy}`}>
+                    <i class="fab fa-github" clannName={LandingCSS.icon}></i>
+                    <i class="fab fa-linkedin" clannName={LandingCSS.icon}></i>
+                    <i class="fas fa-file" clannName={LandingCSS.icon}></i>
+                </div>
             </div>
-        </section>
+        </section >
     )
 }
 
